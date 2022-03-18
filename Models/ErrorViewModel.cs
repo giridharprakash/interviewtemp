787c71fa0ec6652ed7cd8b2c2a6008749191d139
Giridhar Yellapu <giridharprakash@hotmail.com>
2022-03-18 14:24:07 -0400

    Initial project setup for web app.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace interviewtemp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
